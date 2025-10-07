bca310c1b277c29fde0d1e0a8a4525262d697d9e
Jeet <113221510+JEETDESAI25@users.noreply.github.com>
2025-10-06 23:34:17 -0700

    Refactor handlers to use application struct for improved logging and organization. Updated handler functions to methods of the application type and integrated structured logging for error handling.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,6 +8,10 @@ import (
 	"os"
 )
 
+type application struct {
+	logger *slog.Logger
+}
+
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 
@@ -15,21 +19,24 @@ func main() {
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
+	app := &application{
+		logger: logger,
+	}
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-	mux.HandleFunc("GET /snippet/create", snippetCreate)
-	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
+	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
+	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
 	log.Printf("starting server on %s", *addr)
 
 	err := http.ListenAndServe(*addr, mux)
-
 	logger.Error(err.Error())
-	log.Fatal(err)
+	os.Exit(1)
 }
