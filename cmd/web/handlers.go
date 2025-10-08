1c3053a1c1584da9c44b924f6ff58de5885538c4
Jeet <113221510+JEETDESAI25@users.noreply.github.com>
2025-10-08 00:52:31 -0700

    Refactor handlers to utilize newTemplateData function for improved template data management; update templateData struct to include CurrentYear for dynamic rendering in home and snippet view templates.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -18,14 +18,16 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	app.render(w, r, http.StatusOK, "home.tmpl", templateData{
-		Snippets: snippets,
-	})
+	data := app.newTemplateData(r)
+	data.Snippets = snippets
+
+	app.render(w, r, http.StatusOK, "home.tmpl", data)
 
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
+
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
@@ -41,9 +43,10 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	app.render(w, r, http.StatusOK, "view.tmpl", templateData{
-		Snippet: snippet,
-	})
+	data := app.newTemplateData(r)
+	data.Snippet = snippet
+
+	app.render(w, r, http.StatusOK, "view.tmpl", data)
 
 }
 
